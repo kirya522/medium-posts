c09dc863ee4e3b64d53051620d6d24c04ff90841
Kirill Grischuk <kirya522@gmail.com>
2021-03-13 20:41:48 +0500

    init version

---
+++ spring-metrics/src/metricsdemo/src/test/java/com/example/metricsdemo/MetricsDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package com.example.metricsdemo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricsDemoApplicationTests {
+
+    void contextLoads() {
+    }
+
+}
