edca8c44198d6a68bb64cadac618aff487527af8
Kirill Grishchuk <kirya522@gmail.com>
2022-05-22 03:05:49 +0400

    Added direct call to integrations video

---
+++ java/integrations/src/main/kotlin/tech/kirya522/integrations/IntegrationsApplication.kt
@@ -0,0 +1,11 @@
+package tech.kirya522.integrations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IntegrationsApplication
+
+fun main(args: Array<String>) {
+	runApplication<IntegrationsApplication>(*args)
+}
