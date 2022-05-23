5a8d6eae5de93c1edf58e5810ac834b161335e93
Kirill Grishchuk <kirya522@gmail.com>
2022-05-24 00:07:54 +0400

    Made new sender via Event and async

--- java/integrations/src/main/kotlin/tech/kirya522/integrations/apis/IntegrationController.kt
+++ java/integrations/src/main/kotlin/tech/kirya522/integrations/apis/IntegrationController.kt
@@ -1,12 +1,18 @@
 package tech.kirya522.integrations.apis
 
+import org.springframework.context.ApplicationEventPublisher
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import tech.kirya522.integrations.events.WeatherPosterEvent
 import tech.kirya522.integrations.services.WeatherPoster
+import java.util.concurrent.CompletableFuture
 
 @RestController("/")
-class IntegrationController(private val weatherPoster: WeatherPoster) {
+class IntegrationController(
+    private val applicationEventPublisher: ApplicationEventPublisher,
+    private val weatherPoster: WeatherPoster
+) {
 
     @PutMapping("sendMessageViaEvent")
     fun sendMessageViaEvent(
@@ -16,12 +22,17 @@ class IntegrationController(private val weatherPoster: WeatherPoster) {
             defaultValue = "Hi from integrations!"
         ) msg: String
     ) {
-
+        applicationEventPublisher.publishEvent(WeatherPosterEvent(this, msg))
     }
 
     @PutMapping("sendMessageAsTask")
     fun sendMessageAsTask(@RequestParam("msg", required = false, defaultValue = "Hi from integrations!") msg: String) {
 
+    }
+
+    @PutMapping("sendMessageAsync")
+    fun sendMessageAsync(@RequestParam("msg", required = false, defaultValue = "Hi from integrations!") msg: String) {
+        CompletableFuture.runAsync { weatherPoster.sendMessage(msg) }
     }
 
     @PutMapping("/sendMessageViaDirectCall")
