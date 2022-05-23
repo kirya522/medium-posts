45444ab5b40b9d6ae710c883a141c3af824500c9
Kirill Grishchuk <kirya522@gmail.com>
2022-05-24 00:07:54 +0400

    Made new sender via Event and async

--- java/integrations/src/main/kotlin/tech/kirya522/integrations/IntegrationsApplication.kt
+++ java/integrations/src/main/kotlin/tech/kirya522/integrations/IntegrationsApplication.kt
@@ -2,7 +2,9 @@ package tech.kirya522.integrations
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @SpringBootApplication
 class IntegrationsApplication
 
