ce8f74c69b1fdb8d13323f192f87cbc140131205
Kirill Grishchuk <kirya522@gmail.com>
2023-03-19 19:34:36 +0400

    Feature: Added testcontainers

--- java/interview-questions/src/test/java/easy/tests/TestContainersIT.java
+++ java/interview-questions/src/test/java/easy/tests/TestContainersIT.java
@@ -1,6 +1,7 @@
 package easy.tests;
 
 
+import easy.itconfigs.ConditionalOnBeanTestConfig;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,14 +9,16 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
+@SpringBootTest(classes = {ConditionalOnBeanTestConfig.class})
 @Testcontainers
 public class TestContainersIT {
+    private final static String POSTGRES_VERSION="postgres:13.10";
     @Container
-    private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer();
+    private PostgreSQLContainer postgresqlContainer = new PostgreSQLContainer(POSTGRES_VERSION);
 
     /**
      * https://www.testcontainers.org/test_framework_integration/junit_5/
+     * https://www.docker.com/
      */
     @Test
     public void dockerTestContainers_Demo(){
