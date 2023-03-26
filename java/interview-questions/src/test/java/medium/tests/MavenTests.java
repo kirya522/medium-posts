5a354c5aa748ef952bfc0e97fded80a705361b96
Kirill Grishchuk <kirya522@gmail.com>
2023-03-26 17:52:45 +0400

    Feature: Added maven tests

---
+++ java/interview-questions/src/test/java/medium/tests/MavenTests.java
@@ -0,0 +1,13 @@
+package medium.tests;
+
+public class MavenTests {
+
+    /**
+     * https://www.digitalocean.com/community/tutorials/maven-dependency-tree-resolving-conflicts
+     * https://www.baeldung.com/spring-maven-bom
+     */
+    public void showDependencyTree(){
+        // mvn dependency:tree
+        // check parent dependencies
+    }
+}
