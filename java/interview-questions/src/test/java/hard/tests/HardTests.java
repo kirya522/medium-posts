28c16644de6eee8727edf3b93d583a22ca92f9f4
Kirill Grishchuk <kirya522@gmail.com>
2023-04-17 10:21:49 +0400

    feat: IO tests

---
+++ java/interview-questions/src/test/java/hard/tests/HardTests.java
@@ -0,0 +1,49 @@
+package hard.tests;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * IO tests
+ */
+public class HardTests {
+
+    @Test
+    public void blockingIoRead() {
+
+    }
+
+    @Test
+    public void blockingIoWrite() {
+
+    }
+
+    @Test
+    public void bufferRead() {
+
+    }
+
+    @Test
+    public void basicHttpClient() {
+
+    }
+
+    @Test
+    public void monitorHttpClient() {
+        // request + response
+    }
+
+    @Test
+    public void keepAliveHttpClient() {
+
+    }
+
+    @Test
+    public void restTemplateExample() {
+
+    }
+
+    @Test
+    public void webClientExample() {
+
+    }
+}
