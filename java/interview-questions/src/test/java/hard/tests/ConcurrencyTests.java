34b2f01127cd7c1130c87d2954030ecdb34ba310
Kirill Grishchuk <kirya522@gmail.com>
2023-04-17 16:43:19 +0400

    feat: Added more tests

---
+++ java/interview-questions/src/test/java/hard/tests/ConcurrencyTests.java
@@ -0,0 +1,20 @@
+package hard.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class ConcurrencyTests {
+    @Test
+    public void parallelStreamBasic() {
+
+    }
+
+    @Test
+    public void parallelStreamWithSpecifiedPool() {
+
+    }
+
+    @Test
+    public void synchronizedVsLocksTest(){
+
+    }
+}
---
+++ java/interview-questions/src/test/java/hard/tests/SpringTests.java
@@ -0,0 +1,30 @@
+package hard.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class SpringTests {
+    @Test
+    public void cyclicDependencyTest() {
+
+    }
+
+    @Test
+    public void wrappersCombinationTests() {
+
+    }
+
+    @Test
+    public void sqlMigrationTests() {
+
+    }
+
+    @Test
+    public void springDataOverloadTests() {
+
+    }
+
+    @Test
+    public void springDataConnectionLeakTests() {
+
+    }
+}
