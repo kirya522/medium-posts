e3bd5477c6cdb763288b1ec3ccd0d2c5b992d0d3
Kirill Grishchuk <kirya522@gmail.com>
2023-04-19 13:51:52 +0400

    feat: Added IO Tests

--- java/interview-questions/src/test/java/hard/tests/IOTests.java
+++ java/interview-questions/src/test/java/hard/tests/IOTests.java
@@ -2,24 +2,69 @@ package hard.tests;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+
 /**
  * IO tests
  */
 public class IOTests {
 
-    @Test
-    public void blockingIoRead() {
+    private static final String FILE_NAME = "example.txt";
 
+    @Test
+    public void blockingIoWriteAndRead() throws IOException {
+        String text = "Hello, World!";
+
+        try (FileOutputStream fos = new FileOutputStream(FILE_NAME)) {
+            byte[] bytes = text.getBytes();
+            fos.write(bytes);
+            System.out.println("File has been written.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Files.delete(Path.of(FILE_NAME));
     }
 
     @Test
-    public void blockingIoWrite() {
+    public void nioWriteAndRead() throws IOException {
+        Path filePath = Paths.get(FILE_NAME);
+        String text = "Hello, World!";
+        ByteBuffer buffer = ByteBuffer.wrap(text.getBytes());
 
-    }
+        try (FileChannel fileChannel = FileChannel.open(filePath, StandardOpenOption.WRITE, StandardOpenOption.CREATE)) {
+            while (buffer.hasRemaining()) {
+                fileChannel.write(buffer);
+            }
+            System.out.println("File has been written.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            String content = Files.readString(filePath);
+            System.out.println(content);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-    @Test
-    public void bufferRead() {
-
+        Files.delete(filePath);
     }
 
     @Test
