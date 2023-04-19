a5f5a61ea40acfa60548d64dc1fd7c3625c6aedd
Kirill Grishchuk <kirya522@gmail.com>
2023-04-19 12:49:46 +0400

    feat: Added info sources

--- java/interview-questions/src/test/java/hard/tests/ConcurrencyTests.java
+++ java/interview-questions/src/test/java/hard/tests/ConcurrencyTests.java
@@ -10,6 +10,11 @@ import java.util.List;
 import java.util.concurrent.ForkJoinPool;
 
 public class ConcurrencyTests {
+    /**
+     * https://www.baeldung.com/java-when-to-use-parallel-stream
+     * https://www.javatpoint.com/parallel-stream-in-java
+     * https://docs.oracle.com/javase/tutorial/collections/streams/parallelism.html
+     */
     @Test
     public void parallelStreamBasic() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
@@ -20,6 +25,10 @@ public class ConcurrencyTests {
                 .forEach(System.out::println);
     }
 
+    /**
+     * https://www.baeldung.com/java-8-parallel-streams-custom-threadpool
+     * https://medium.com/geekculture/pitfalls-of-java-parallel-streams-731fe0c1eb5f
+     */
     @Test
     public void parallelStreamWithSpecifiedPool() {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
@@ -36,6 +45,11 @@ public class ConcurrencyTests {
         customThreadPool.shutdown();
     }
 
+    /**
+     * https://www.geeksforgeeks.org/lock-framework-vs-thread-synchronization-in-java/
+     * https://winterbe.com/posts/2015/04/30/java8-concurrency-tutorial-synchronized-locks-examples/
+     * https://www.baeldung.com/java-concurrent-locks
+     */
     @Test
     public void synchronizedVsLocksTest() {
         ReentrantLockExample lockExample = new ReentrantLockExample();
