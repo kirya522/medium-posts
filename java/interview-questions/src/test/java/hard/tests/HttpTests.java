310bbeb45b5ffd958163c110afab7a48a65f16ff
Kirill Grishchuk <kirya522@gmail.com>
2023-04-19 19:49:16 +0400

    feat: Added Http tests

--- java/interview-questions/src/test/java/hard/tests/HttpTests.java
+++ java/interview-questions/src/test/java/hard/tests/HttpTests.java
@@ -1,11 +1,78 @@
 package hard.tests;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.util.EntityUtils;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HttpTests {
     @Test
     public void basicHttpClient() {
+        try {
+            URL url = new URL("http://example.com/api");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
 
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            System.out.println(response);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void apacheHttpClient() {
+        try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+            HttpGet request = new HttpGet("http://example.com/api");
+
+            HttpResponse response = httpClient.execute(request);
+
+            String responseBody = EntityUtils.toString(response.getEntity());
+
+            System.out.println(responseBody);
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void restTemplateExample() {
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("param1", "value1");
+        map.add("param2", "value2");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("http://example.com/api", HttpMethod.POST, request, String.class);
+
+        System.out.println(response.getBody());
     }
 
     @Test
@@ -15,16 +82,31 @@ public class HttpTests {
 
     @Test
     public void keepAliveHttpClient() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(100);
+        connectionManager.setDefaultMaxPerRoute(20);
+        connectionManager.setValidateAfterInactivity(5000);
 
-    }
+        CloseableHttpClient httpClient = HttpClients.custom()
+                .setConnectionManager(connectionManager)
+                .setKeepAliveStrategy(DefaultConnectionKeepAliveStrategy.INSTANCE)
+                .build();
 
-    @Test
-    public void restTemplateExample() {
+        HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory(httpClient);
 
-    }
+        RestTemplate restTemplate = new RestTemplate(factory);
 
-    @Test
-    public void webClientExample() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("param1", "value1");
+        map.add("param2", "value2");
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<String> response = restTemplate.exchange("http://example.com/api", HttpMethod.POST, request, String.class);
+
+        System.out.println(response.getBody());
     }
 }
