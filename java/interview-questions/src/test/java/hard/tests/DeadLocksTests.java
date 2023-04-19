53a470c5ae883c221cdb70470670980086187000
Kirill Grishchuk <kirya522@gmail.com>
2023-04-19 14:33:06 +0400

    feat: Added tests for deadlocks

--- java/interview-questions/src/test/java/hard/tests/DeadLocksTests.java
+++ java/interview-questions/src/test/java/hard/tests/DeadLocksTests.java
@@ -1,25 +1,61 @@
 package hard.tests;
 
+import hard.classes.DeadlockExample;
+import hard.classes.NestedLocksExample;
+import hard.classes.TryLockExample;
 import org.junit.jupiter.api.Test;
 
+/**
+ * https://www.baeldung.com/java-deadlock-livelock
+ * https://www.javatpoint.com/deadlock-in-java
+ * https://www.digitalocean.com/community/tutorials/deadlock-in-java-example
+ */
 public class DeadLocksTests {
     @Test
-    public void nestedLocksDeadLock() {
+    public void nestedLocksDeadLock() throws InterruptedException {
+        DeadlockExample deadlockExample = new DeadlockExample();
 
+        deadlockExample.synchronizedDeadLock();
     }
 
     @Test
-    public void cyclicBarrier() {
+    public void cyclicBarrier() throws InterruptedException {
+        DeadlockExample deadlockExample = new DeadlockExample();
 
+        deadlockExample.cyclicBarrierDeadLock();
     }
 
     @Test
-    public void nestedLocksCorrect(){
-
+    public void nestedLocksCorrect() {
+        NestedLocksExample example = new NestedLocksExample();
+        for (int i = 0; i < 100; i++) {
+            new Thread(() -> {
+                example.method1();
+                example.method2();
+            }).start();
+        }
+        System.out.println("No dead locks");
     }
 
     @Test
     public void nestedLocksTryLock() {
+        TryLockExample tryLockExample = new TryLockExample();
+        new Thread(() -> {
+            try {
+                tryLockExample.method1();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
+        new Thread(() -> {
+            try {
+                tryLockExample.method2();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
+
+        System.out.println("Deadlock resolved");
     }
 }
