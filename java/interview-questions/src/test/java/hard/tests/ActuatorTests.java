df3b163acb87edf312a60916ccd43bc78dfc2d94
Kirill Grishchuk <kirya522@gmail.com>
2023-04-27 19:49:17 +0400

    feat: Added more links

--- java/interview-questions/src/test/java/hard/tests/ActuatorTests.java
+++ java/interview-questions/src/test/java/hard/tests/ActuatorTests.java
@@ -2,6 +2,9 @@ package hard.tests;
 
 import org.junit.jupiter.api.Test;
 
+/**
+ * https://docs.spring.io/spring-boot/docs/current/reference/html/actuator.html
+ */
 public class ActuatorTests {
     @Test
     public void actuatorTest() {
