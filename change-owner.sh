7f9ec16cc7068bec705dfcb39a9004b0e811a272
Kirill Grishchuk <kirya522@gmail.com>
2023-04-28 00:07:28 +0400

    fixed script

---
+++ change-owner.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+ 
+git filter-branch --env-filter '
+ 
+OLD_EMAIL="old@email.com"
+CORRECT_NAME="Kirill Grishchuk"
+CORRECT_EMAIL="kirya522@gmail.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
