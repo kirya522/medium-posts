5b08f7ff743978a24a965740207c4a0ad27d4680
Kirill Grishchuk <kirya522@gmail.com>
2025-01-01 22:32:36 +0500

    Создание и оптимизация данныъ в SQL

--- general/db/sql-optimization/requests.sql
+++ general/db/sql-optimization/requests.sql
@@ -2,107 +2,86 @@
 -- Как написать оптимальный запрос и ускорить его выполнение
 ---
 
--- 1. поиск клиента
+-- 1.1. поиск по id -- первичный ключ
+-- Поиск клиента
 EXPLAIN SELECT * FROM customers
-WHERE customer_id = 7565;
+WHERE customer_id = 145065;
 
 EXPLAIN ANALYZE SELECT * FROM customers
-WHERE customer_id = 7565;
+WHERE customer_id = 145065;
 
-CREATE INDEX idx_customer_customer_id ON customers(customer_id);
+CREATE INDEX idx_customers_customer_id ON customers(customer_id);
 
 
--- 2. поиск заказов клиента
+-- 1.2. поиск по id -- внешний ключ
+-- Поиск заказов клиента
 EXPLAIN SELECT * FROM orders
-WHERE customer_id = 7565;
+WHERE customer_id = 145065;
 
 EXPLAIN ANALYZE SELECT * FROM orders
-WHERE customer_id = 7565;
+WHERE customer_id = 145065;
 
-CREATE INDEX idx_customer_id ON orders(customer_id);
-
-
--- 3. Сколько заказов по дням
-EXPLAIN SELECT COUNT(*), order_date 
-FROM orders 
-WHERE order_date > '2024-01-01'
-GROUP BY order_date;
-
-EXPLAIN ANALYZE SELECT COUNT(*), order_date 
-FROM orders 
-WHERE order_date > '2024-01-01'
-GROUP BY order_date;
-
-CREATE INDEX idx_order_date ON orders(order_date);
+CREATE INDEX idx_orders_customer_id ON orders(customer_id);
 
 
--- 4. Поиск клиента по имени или фамилии
-EXPLAIN SELECT *
-FROM customers 
-WHERE first_name like '%Custo%';
-
-EXPLAIN ANALYZE SELECT *
-FROM customers 
-WHERE first_name like '%Custo%';
-
-SELECT *
-FROM customers 
-WHERE first_name @@ to_tsquery('english', 'Custo');
-
-CREATE INDEX idx_full_text_name ON customers USING GIN (to_tsvector('english', first_name || ' ' || last_name));
-
-
--- 5. Поиск клиентов, которые находятся в радиусе 100 км от Нью-Йорка
-EXPLAIN SELECT customer_id, first_name, last_name, email
-FROM customers
-WHERE ST_DWithin(
-    location, 
-    ST_SetSRID(ST_MakePoint(-73.935242, 40.730610), 4326),  -- Нью-Йорк (долгота, широта)
-    100000  -- Радиус в метрах (100 км = 100000 метров)
-);
-
-EXPLAIN ANALYZE SELECT customer_id, first_name, last_name, email
-FROM customers
-WHERE ST_DWithin(
-    location, 
-    ST_SetSRID(ST_MakePoint(-73.935242, 40.730610), 4326),  -- Нью-Йорк (долгота, широта)
-    100000  -- Радиус в метрах (100 км = 100000 метров)
-);
-
-CREATE INDEX idx_location ON customers USING GIST (location);
-
-
--- 6. Поиск пользователя по email
+-- 1.3. поиск по email - hash-idx
+-- Поиск пользователя по email
 EXPLAIN SELECT * FROM customers
 WHERE email = 'customer_7565@example.com';
 
 EXPLAIN ANALYZE SELECT * FROM customers
 WHERE email = 'customer_7565@example.com';
 
-CREATE INDEX idx_hash_email ON customers USING HASH (email);
+CREATE INDEX idx_customers_hash_email ON customers USING HASH (email);
 
---TODO проверить результат
 EXPLAIN SELECT * FROM customers
 WHERE email > 'customer_75';
 
+drop index idx_customers_hash_email;
 
--- 7. Есть индексы на соединяемых таблицах
+CREATE INDEX idx_customers_email ON customers(email);
+
+explain ANALYZE SELECT * FROM customers
+WHERE email > 'customer_75';
+
+
+-- 1.4. поиск по тексту
+-- Поиск клиента по имени или фамилии
+EXPLAIN SELECT *
+FROM customers 
+WHERE first_name like 'Custo1%';
+
+EXPLAIN ANALYZE SELECT *
+FROM customers 
+WHERE first_name like 'Custo1%';
+
+CREATE INDEX idx_customers_first_name ON customers(first_name);
+
+EXPLAIN ANALYZE SELECT *
+FROM customers 
+WHERE first_name like 'Custo1%';
+
+CREATE EXTENSION pg_trgm;
+CREATE INDEX idx_customers_full_text_name ON customers  USING gin (first_name gin_trgm_ops);
+
+
+-- 2. Есть индексы на соединяемых таблицах
 EXPLAIN SELECT o.order_id, c.first_name 
 FROM orders o
-INNER JOIN customers c ON o.customer_id = c.customer_id
-WHERE o.order_date > '2024-01-01';
+JOIN customers c ON o.customer_id = c.customer_id
+WHERE o.order_date > '2024-12-01';
 
 EXPLAIN ANALYZE SELECT o.order_id, c.first_name 
 FROM orders o
-INNER JOIN customers c ON o.customer_id = c.customer_id
-WHERE o.order_date > '2024-01-01';
+JOIN customers c ON o.customer_id = c.customer_id
+WHERE o.order_date > '2024-12-30';
 
 
--- 8. Брать только нужные данные
+-- 3. Брать только нужные данные
 EXPLAIN ANALYZE SELECT *
 FROM customers
-WHERE email = 'customer_7565@example.com';
+WHERE email like 'customer_7565%@example.com';
 
-EXPLAIN SELECT first_name, last_name 
+explain ANALYZE SELECT first_name 
 FROM customers
-WHERE email = 'customer_7565@example.com';
+WHERE email like 'customer_7565%@example.com';
--- general/db/sql-optimization/schema.sql
+++ general/db/sql-optimization/schema.sql
@@ -4,8 +4,7 @@ CREATE TABLE customers (
     customer_id SERIAL, -- специально без PRIMARY KEY иначе будет индекс
     email VARCHAR(100), -- специально без UNIQUE иначе будет индекс
     first_name VARCHAR(50),
-    last_name VARCHAR(50),
-    location GEOMETRY(Point, 4326)
+    last_name VARCHAR(50)
 );
 
 -- Создание таблицы заказов
@@ -18,26 +17,19 @@ CREATE TABLE orders (
 );
 
 -- Генерация клиентов
-INSERT INTO customers (first_name, last_name, email, location)
+INSERT INTO customers (first_name, last_name, email)
 SELECT
     'Customer_' || g.i,
     'LastName_' || g.i,
-    'customer_' || g.i || '@example.com',
-    ST_SetSRID(
-        ST_MakePoint(
-            (RANDOM() * 360) - 180,  -- Генерация случайной долготы в диапазоне от -180 до 180
-            (RANDOM() * 180) - 90     -- Генерация случайной широты в диапазоне от -90 до 90
-        ),
-        4326  -- Устанавливаем SRID (система координат WGS 84)
-    )
-FROM generate_series(1, 10001) AS g(i)
+    'customer_' || g.i || '@example.com'
+FROM generate_series(1, 400001, 1) AS g(i);
 
 -- Генерация заказов
 INSERT INTO orders (customer_id, order_date, amount, status)
 SELECT
-    (RANDOM() * 10000)::INT + 1,
+    (RANDOM() * 400000)::INT + 1,
     '2024-01-01'::DATE + (RANDOM() * 365)::INT,
     (RANDOM() * 500)::NUMERIC(10, 2),
     CASE WHEN RANDOM() > 0.5 THEN 'completed' ELSE 'pending' END
-FROM generate_series(1, 1000000);
+FROM generate_series(1, 40_000_000);
 
