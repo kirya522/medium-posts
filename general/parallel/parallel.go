714f919c2da44df83d849c3785bc1bee886641b7
Kirill Grischuk <kdgrischuk@avito.ru>
2025-02-01 01:27:08 +0500

    Добавил тесты на гонки

--- general/parallel/parallel.go
+++ general/parallel/parallel.go
@@ -1,5 +1,38 @@
 package parallel
 
-func empty() int {
-	return 0
+import "sync"
+
+func parallelCount() int {
+	var counter int
+	var wg sync.WaitGroup
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			counter++ // Гонка данных!
+		}()
+	}
+
+	wg.Wait()
+	return counter
+}
+
+func parallelCountFix() int {
+	var counter int
+	var wg sync.WaitGroup
+	var mu sync.Mutex
+
+	for i := 0; i < 10; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			mu.Lock()
+			counter++ // Решение через мьютекс
+			mu.Unlock()
+		}()
+	}
+
+	wg.Wait()
+	return counter
 }
--- general/parallel/parallel_test.go
+++ general/parallel/parallel_test.go
@@ -1,48 +1,28 @@
 package parallel
 
 import (
-	"math/rand"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 // Race
-
 func TestDataRace(t *testing.T) {
-	var counter int
-	var wg sync.WaitGroup
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			counter++ // Гонка данных!
-		}()
+	tries := 100
+	for i := 0; i < tries; i++ {
+		counter := parallelCount()
+		assert.Equal(t, 10, counter)
 	}
-
-	wg.Wait()
-	t.Logf("Final counter: %d", counter)
-	// Запустите этот тест с флагом -race: go test -race
 }
 
-func TestDataRaceSolution(t *testing.T) {
-	var counter int
-	var wg sync.WaitGroup
-	var mu sync.Mutex
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			mu.Lock()
-			counter++ // Решение через мьютекс
-			mu.Unlock()
-		}()
+func TestDataRaceFix(t *testing.T) {
+	tries := 100
+	for i := 0; i < tries; i++ {
+		counter := parallelCountFix()
+		assert.Equal(t, 10, counter)
 	}
-
-	wg.Wait()
-	t.Logf("Final counter: %d", counter)
 }
 
 // Deadlock
@@ -83,7 +63,7 @@ func TestDeadlock(t *testing.T) {
 	// Этот тест застрянет из-за deadlock
 }
 
-func TestDeadlockSolution(t *testing.T) {
+func TestDeadlockFix(t *testing.T) {
 	var mu1, mu2 sync.Mutex
 	var wg sync.WaitGroup
 
@@ -116,145 +96,4 @@ func TestDeadlockSolution(t *testing.T) {
 	}()
 
 	wg.Wait()
-	// Этот тест застрянет из-за deadlock
-}
-
-// Голодание потоков
-
-func TestStarvation(t *testing.T) {
-	var mu sync.Mutex
-	var wg sync.WaitGroup
-
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		mu.Lock()
-		defer mu.Unlock()
-		t.Log("Long operation started")
-		// Долгая операция
-		time.Sleep(3 * time.Second)
-		t.Log("Long operation finished")
-	}()
-
-	go func() {
-		defer wg.Done()
-		time.Sleep(500 * time.Millisecond) // Небольшая задержка перед попыткой захвата
-		mu.Lock()
-		defer mu.Unlock()
-		t.Log("Short operation finished")
-	}()
-
-	start := time.Now()
-	wg.Wait()
-	t.Logf("Execution completed in %v", time.Since(start))
-}
-
-func TestStarvationSolution(t *testing.T) {
-	var mu sync.Mutex
-	var cond = sync.NewCond(&mu)
-	var wg sync.WaitGroup
-	isLongDone := false
-
-	wg.Add(2)
-
-	go func() {
-		defer wg.Done()
-		mu.Lock()
-		t.Log("Long operation started")
-		// Долгая операция
-		time.Sleep(3 * time.Second)
-		isLongDone = true
-		cond.Broadcast() // Уведомляем, что операция завершена
-		mu.Unlock()
-		t.Log("Long operation finished")
-	}()
-
-	go func() {
-		defer wg.Done()
-		mu.Lock()
-		for !isLongDone {
-			cond.Wait() // Ждём завершения долгой операции
-		}
-		t.Log("Short operation finished")
-		mu.Unlock()
-	}()
-
-	start := time.Now()
-	wg.Wait()
-	t.Logf("Execution completed in %v", time.Since(start))
-}
-
-// performance issues
-
-func TestLatencyAndInstability(t *testing.T) {
-	sharedMap := make(map[int]int)
-	var mu sync.Mutex
-	var wg sync.WaitGroup
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(id int) {
-			defer wg.Done()
-			for j := 0; j < 100; j++ {
-				mu.Lock()
-				sharedMap[rand.Intn(100)] = id
-				mu.Unlock()
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(10)))
-			}
-		}(i)
-	}
-
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func(id int) {
-			defer wg.Done()
-			for j := 0; j < 100; j++ {
-				mu.Lock()
-				_ = sharedMap[rand.Intn(100)]
-				mu.Unlock()
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(10)))
-			}
-		}(i)
-	}
-
-	start := time.Now()
-	wg.Wait()
-	t.Logf("Execution completed in %v", time.Since(start))
-}
-
-func TestLatencyAndInstabilitySolution(t *testing.T) {
-	sharedMap := make(map[int]int)
-	var rwMu sync.RWMutex
-	var wg sync.WaitGroup
-
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func(id int) {
-			defer wg.Done()
-			for j := 0; j < 100; j++ {
-				rwMu.Lock()
-				sharedMap[rand.Intn(100)] = id
-				rwMu.Unlock()
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(10)))
-			}
-		}(i)
-	}
-
-	for i := 0; i < 5; i++ {
-		wg.Add(1)
-		go func(id int) {
-			defer wg.Done()
-			for j := 0; j < 100; j++ {
-				rwMu.RLock()
-				_ = sharedMap[rand.Intn(100)]
-				rwMu.RUnlock()
-				time.Sleep(time.Millisecond * time.Duration(rand.Intn(10)))
-			}
-		}(i)
-	}
-
-	start := time.Now()
-	wg.Wait()
-	t.Logf("Execution completed in %v", time.Since(start))
 }
